0ee141378445219980c3eba82120d678511877de
razza <razza@DESKTOP-LOJ4G7F.lan>
2023-01-24 02:53:16 -0500

    commit for switch01

---
+++ My1stJavaProject/src/com/ifelse/my/switch01.java
@@ -0,0 +1,40 @@
+package com.ifelse.my;
+
+public class switch01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day;
+		day=3;
+		switch(day) {
+		case 1:
+			System.out.println("This is Monday");
+			break;
+		case 2:
+			System.out.println("This is Tuesday");
+			break;
+		case 3:
+			System.out.println("This is Wednesday");
+			break;
+		case 4:
+			System.out.println("This is Thrusday");
+			break;
+		case 5:
+			System.out.println("This is Friday");
+			break;
+		case 6:
+			System.out.println("This is Satarday");
+			break;
+		case 7:
+			System.out.println("This is Sunday");
+			break;
+			default:
+			System.out.println("This is unknown");
+			break;
+		
+		
+		}
+
+	}
+
+}
