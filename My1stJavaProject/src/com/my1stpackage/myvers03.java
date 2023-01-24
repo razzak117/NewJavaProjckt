5a5b665e7014148605846c020a70eec9243bf01b
razza <razza@DESKTOP-LOJ4G7F.lan>
2023-01-24 02:33:21 -0500

    commit for vers03

---
+++ My1stJavaProject/src/com/my1stpackage/myvers03.java
@@ -0,0 +1,51 @@
+package com.my1stpackage;
+
+public class myvers03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Quick Brown Fox Jumps Over The Lazy Dog");
+		System.out.println("*************");
+		String res="Quick";
+		res +=" ";
+		res +="Brown Fox";
+		res +=" ";
+		res +="Jumps";
+		res +=" ";
+		res +="Over";
+		res +=" ";
+		res +=" The Lazy Dog";
+		res +=".";
+		System.out.println(res);
+		System.out.println("Bangladesh is a beautifull country in the world");
+		System.out.println(99999);
+		String msg="Bangladesh";
+		msg +=" ";
+		msg +="is";
+		msg +=" ";
+		msg +="a";
+		msg +=" ";
+		msg +="beautifull";
+		msg +=" ";
+		msg +="country";
+		msg +="in the world";
+		System.out.println(msg);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
