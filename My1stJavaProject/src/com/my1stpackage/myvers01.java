c9ae7011ff0cdb345fd9d435c3cbdea8d532450b
razza <razza@DESKTOP-LOJ4G7F.lan>
2023-01-24 02:30:09 -0500

    commit for vers01

---
+++ My1stJavaProject/src/com/my1stpackage/myvers01.java
@@ -0,0 +1,24 @@
+package com.my1stpackage;
+
+public class myvers01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x="Hello";
+		String y="World";
+		int A=30;
+		int B=50;
+		System.out.println(A + B);
+		System.out.println(x+" "+y);
+		System.out.println(A+B+x+y);
+		System.out.println(x+y+A+B);
+		System.out.println(x+y+(A+B));
+		System.out.println(x+B+y+A);
+		System.out.println(A+B+x+y+B+A);
+		
+		
+		
+
+	}
+
+}
