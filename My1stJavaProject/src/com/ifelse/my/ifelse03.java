1c46f65cb2bb46a69cb40fa8bcefac82df389677
razza <razza@DESKTOP-LOJ4G7F.lan>
2023-01-24 02:48:37 -0500

    commit for ifelse03

---
+++ My1stJavaProject/src/com/ifelse/my/ifelse03.java
@@ -0,0 +1,32 @@
+package com.ifelse.my;
+
+public class ifelse03 {
+
+				public static void main(String[]args) {
+			
+			int x,y,z;
+			x=400; 
+			y=500;
+			z=300;
+			if(x<y & x<z) {
+				System.out.println("x is smaller number");
+			}else if(y<z){
+				System.out.println("y is smaller number");
+			}else {
+				System.out.println("z is smaller number");
+			
+			}
+		}
+	}
+
+			
+
+				
+			
+		
+	
+
+
+	
+
+
