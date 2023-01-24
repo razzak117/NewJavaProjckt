d55a9e87e136b5cfbcefbf5a874ac6ca08abd5db
razza <razza@DESKTOP-LOJ4G7F.lan>
2023-01-24 03:50:51 -0500

    commit for switch02

---
+++ My1stJavaProject/src/com/ifelse/my/switch02.java
@@ -0,0 +1,51 @@
+package com.ifelse.my;
+
+public class switch02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Month=8;
+		switch(Month) {
+		case 1:
+			System.out.println("First month of the year");
+		break;
+		case 2:
+		System.out.println("second month of the year");
+		break;
+		case 3:
+			System.out.println("Third month of the year");
+		break;
+		case 4:
+			System.out.println("Forth month of the year");
+		break;
+		case 5:
+			System.out.println("Fifth month of the year");
+		case 6:
+			System.out.println("Sixth month of the year");
+		break;
+		case 7:
+		System.out.println("Eeventh month of the year");
+		break;
+		case 8:
+			System.out.println("Eighth month of the year");
+		break;
+		case 9:
+			System.out.println("Ninth month of the year");
+		break;
+		case 10:
+			System.out.println("Tenth month of the year");
+		case 11:
+			System.out.println("Eleventh month of the year");
+		break;
+		case 12:
+		System.out.println("Twelveth month of the year");
+		break;
+		default:
+			System.out.println("Unknown month of the year");
+		
+		
+		}
+
+	}
+
+}
