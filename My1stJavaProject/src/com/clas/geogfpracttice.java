417f35999dd14575836ffcebd2fbf6d6cafaea95
razza <razza@DESKTOP-LOJ4G7F.lan>
2023-01-31 09:19:53 -0500

    commit for geogfpractice

---
+++ My1stJavaProject/src/com/clas/geogfpracttice.java
@@ -0,0 +1,50 @@
+package com.clas;
+
+public class geogfpracttice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		geogf gfy1=new geogf ();
+		geogf gfy2=new geogf ();
+		geogf gfy3=new geogf ();
+		geogf gfy4=new geogf ();
+		
+		gfy1.lName="Abdul";
+		gfy1.fName="Jabbar";
+		gfy1.post="Office Asst";
+		gfy1.dist="Khulna";
+		gfy1.cell=198426378;
+		
+		gfy2.lName="Ali";
+		gfy2.fName="Mohammed";
+		gfy2.post="sr.Sales";
+		gfy2.dist="Bogra";
+		gfy2.cell=1811426378;
+		
+		gfy3.lName="Islam";
+		gfy3.fName="Nazrul";
+		gfy3.post="Sales";
+		gfy3.dist="Dhaka";
+		gfy3.cell=1711423678;
+		
+		gfy4.lName="Rahman";
+		gfy4.fName="Abdur";
+		gfy4.post="Manager";
+		gfy4.dist="Dhaka";
+		gfy4.cell=1825623678;
+		
+		/*gfy5.lName="Hossain";
+		gfy5.fName="Eyamin";
+		gfy5.post="Sales";
+		gfy5.dist="Dhaka";
+		gfy5.cell=1711083578;*/
+
+		System.out.println(gfy1.lName+" "+ gfy1.fName+", "+gfy1.post+", "+"Address:"+" "+gfy1.dist+","+"Cell no:"+gfy1.cell+".");
+		System.out.println(gfy2.lName+" "+ gfy2.fName+", "+gfy2.post+", "+"Address:"+" "+gfy2.dist+","+"Cell no:"+gfy2.cell+".");
+		System.out.println(gfy3.lName+" "+ gfy3.fName+", "+gfy3.post+", "+"Address:"+" "+gfy3.dist+","+"Cell no:"+gfy3.cell+".");
+		System.out.println(gfy4.lName+" "+ gfy4.fName+", "+gfy4.post+", "+"Address:"+" "+gfy4.dist+","+"Cell no:"+gfy4.cell+".");
+	 
+
+	}
+
+}
